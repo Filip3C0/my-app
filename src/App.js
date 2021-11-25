3b0cf1f5c3e4914be20c211d77a43726521bb653
Filipe Costa <filipecnht@gmail.com>
2021-11-25 14:16:04 -0400

    Project react started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import { createClient } from 'pexels';
+import React from 'react';
+
+
+
+
+
+const client =createClient('563492ad6f91700001000001b5af10ca0a7d4280a6263b2630883c1a');
+        
+const query = 'Ocean';
+
+let photo;
+
+class App extends React.Component{
+  
+  constructor(props){
+   
+    super(props);
+    this.state = {photo:'https://images.pexels.com/photos/2014422/pexels-photo-2014422.jpeg'};
+
+    
+  }
+  getPhoto =()=> {
+     
+   client.photos.search({ query, per_page: 4 }).then(photos => {photo = photos; this.setState({ photo: photo.photos[0].src.medium}); });
+    
+    
+  }
+
+  render(){
+    
+    this.getPhoto();  
+    return(
+      
+      <img src={this.state.photo}></img>    
+      )
+    
+  }
+
+  
 }
 
 export default App;
